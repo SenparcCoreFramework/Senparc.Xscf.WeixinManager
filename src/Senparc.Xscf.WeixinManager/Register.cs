4f58e77c8dbb07b814444599bda871213df25cb3
JeffreySu <www.jeffrey.su@gmail.com>
2020-03-14 01:15:47 +0800

    v1.6.7

--- src/Senparc.Xscf.WeixinManager/Register.cs
+++ src/Senparc.Xscf.WeixinManager/Register.cs
@@ -27,7 +27,7 @@ namespace Senparc.Xscf.WeixinManager
         public override string Uid => "EB84CB21-AC22-406E-0001-000000000001";
 
 
-        public override string Version => "0.1.6.2-beta1";
+        public override string Version => "0.1.7-beta1";
 
 
         public override string MenuName => "微信管理";
